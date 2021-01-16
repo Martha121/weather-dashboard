21b4c72e7d43899202cdc51cdcf410eee6ed7df4
MGcommits <marthagamez211@gmail.com>
2021-01-15 21:59:43 -0600

    Add and display UV index

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,21 +1,34 @@
-var cityDataEl=document.getElementById("city-data");
 var arrSearchHistory = [];
 
 // Gets the search history when this file is loaded
 getSearchHistory();
 
-var getCityWeather= function(city){
+function getWeatherByCity(city){
     var apiKey = "id=524901&appid=8d033d7f143f0e2af2920c11bc814694";
     var apiWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&"+apiKey;
-    return fetch(apiWeatherUrl)
-    
+    // return fetch(apiWeatherUrl)
+    fetch(apiWeatherUrl)
     .then(function(response){
         //request was successful
         if (response.ok){
-            response.json().then (function(data) {
+            response.json()
+            .then (function(data) {
                 setSearchHistory(city);
-                displayCityWeather(data);
-                
+                var apiOnecallUrl = "http://api.openweathermap.org/data/2.5/onecall?lat="+data.coord.lat+"&lon="+data.coord.lon+"&"+apiKey;
+                fetch(apiOnecallUrl)
+                .then(function(response){
+                    //request was successful
+                    if (response.ok){
+                        response.json().then (function(onecallData) {
+                            displayCityWeather(data, onecallData);
+                        });
+                    }
+                    else{
+                        // if not successful, redirect to homepage
+                        console.log("Could not get onecall");
+                        document.location.replace("./index.html");
+                    }
+                });
             });
         }
         else{
@@ -25,39 +38,39 @@ var getCityWeather= function(city){
     });
 };
 
-var displayCityWeather=function(data){
+
+var displayCityWeather=function(data, allData){
+    var cityDataEl = document.getElementById("city-data");  
     console.log(data);
-    console.log(data.main.temp);
-    console.log(data.name);
+    console.log(allData);
     cityDataEl.innerHTML="";
     var cityName=document.createElement("p");
     var iconImg = document.createElement("img");
     var cityTemp=document.createElement("p");
     var cityHum=document.createElement("p");
     var cityWind=document.createElement("p");
-    //var cityUVIndex=document.createElement("p");
+    var cityUVIndex=document.createElement("p");
     cityName.textContent=data.name;
     iconImg.src = "http://openweathermap.org/img/wn/"+ data.weather[0].icon +".png";
     cityName.appendChild(iconImg);
     cityTemp.textContent="Temperature: "+data.main.temp+" ⁰F";
     cityHum.textContent="Humidity: "+data.main.humidity+"%";
-    cityWind.textContent="Wind Speed: "+data.main.wind+" MPH";
-    //cityUVIndex.textContent=data.main.humidity;
-    console.log(cityTemp);
+    cityWind.textContent="Wind Speed: "+data.wind.speed+" MPH";
+    cityUVIndex.textContent="UV Index: "+allData.daily[0].uvi;
     cityDataEl.appendChild(cityName);
     cityDataEl.appendChild(cityTemp);
     cityDataEl.appendChild(cityHum);
     cityDataEl.appendChild(cityWind);
+    cityDataEl.appendChild(cityUVIndex);
 }
 
 function searchBttnClicked(){
     var cityName = document.getElementById("city-input").value;
     //console.log(cityName);
-    getCityWeather (cityName);
+    getWeatherByCity(cityName);
 }
 
 function setSearchHistory(cityName){
-    console.log(arrSearchHistory);
     arrSearchHistory.push(cityName);
     window.localStorage.setItem("search_history", JSON.stringify(arrSearchHistory));
 }
